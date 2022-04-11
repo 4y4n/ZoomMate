2edaadec51d2304cfe7c0c4809b71261dd05ce91
Ayan Noorani <93687003+4y4n@users.noreply.github.com>
2022-04-10 21:05:08 -0400

    Add files via upload

---
+++ zoomOpener.py
@@ -0,0 +1,23 @@
+import time
+import datetime
+import json
+import os
+import webbrowser
+
+classes = json.load(open('classData.json'))
+isStarted = False
+for i in classes:
+    date = datetime.datetime.now()
+    if date.strftime("%a") in i["classActive"]:
+        while True:
+            date = datetime.datetime.now()
+            if not isStarted:
+                if date.strftime("%H") == i["startTime"].split(':')[0] and date.strftime("%M") == i["startTime"].split(':')[1]:
+                    webbrowser.open(i["link"])
+                    isStarted = True
+            else:
+                if date.strftime("%H") == i["endTime"].split(':')[0] and date.strftime("%M") == i["endTime"].split(':')[1]:
+                    os.system("taskkill /f /im  Zoom.exe")
+                    isStarted = False
+                    break
+            time.sleep(5)
